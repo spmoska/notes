9d1fd96949982910c5281c42b4c4e3f2b2c6215b
Danil <Dpopov25@yandex.ru>
2022-05-21 13:32:41 +0500

    add script
    current date

--- js/script.js
+++ js/script.js
@@ -1,6 +1,17 @@
 $(document).ready(function() {
+    function date() {
+        let curDate = new Date(),
+        day = curDate.getDate(),
+        month = curDate.getMonth() + 1,
+        year = curDate.getFullYear(),
+        date = day + '.0' + month + '.' + year;
+        return date;
+    }
+
+    $(".current__date").text(date());
     $(".list__item").click(function() {
         $(".note__body__menu", this).toggleClass("active");
         $(".buttons", this).toggleClass("active");
     })
+
 })
